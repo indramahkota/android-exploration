2d4f523f5f57ffa1a5a290a3ea81f1bf7dbd1bab
Indra Mahkota <indramahkota1@gmail.com>
2022-09-04 12:30:34 +0700

    Update patch version to 0.0.3 (build-logic) - add new github action pipeline detekt-diff.yml - separate developing and release branch (develop&main) - only detekt diff when push to develop and detekt all when push to main branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 @Suppress("StringLiteralDuplication")
 plugins {
-    id("com.indramahkota.build.logic.convention.detekt") version "0.0.1"
+    id("com.indramahkota.build.logic.convention.detekt") version "0.0.3"
 
-    id("com.indramahkota.build.logic.convention.android-config") version "0.0.1"
-    id("com.indramahkota.build.logic.convention.android-lib") version "0.0.1" apply false
-    id("com.indramahkota.build.logic.convention.android-app") version "0.0.1" apply false
+    id("com.indramahkota.build.logic.convention.android-config") version "0.0.3"
+    id("com.indramahkota.build.logic.convention.android-lib") version "0.0.3" apply false
+    id("com.indramahkota.build.logic.convention.android-app") version "0.0.3" apply false
 
-    id("com.indramahkota.build.logic.convention.compose-config") version "0.0.1"
-    id("com.indramahkota.build.logic.convention.compose-lib") version "0.0.1" apply false
-    id("com.indramahkota.build.logic.convention.compose-app") version "0.0.1" apply false
+    id("com.indramahkota.build.logic.convention.compose-config") version "0.0.3"
+    id("com.indramahkota.build.logic.convention.compose-lib") version "0.0.3" apply false
+    id("com.indramahkota.build.logic.convention.compose-app") version "0.0.3" apply false
 }
 
 val androidApplicationId by extra { "com.indramahkota.app.exploration" }
@@ -25,6 +25,12 @@ indramahkota {
 
     // Default JavaVersion.VERSION_1_8
     jvmTarget.set(JavaVersion.VERSION_11)
+
+    detekt {
+        checkOnlyDiffWithBranch("main") {
+            fileExtensions = setOf(".kt", ".kts")
+        }
+    }
 
     android {
         minSdk.set(23)
