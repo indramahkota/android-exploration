e6d141e7a19d4875aab2b4ea12e7821079fd1a8b
Indra Mahkota <indramahkota1@gmail.com>
2023-02-28 14:40:29 +0700

    Split navigation to separate extension function

--- app/exploration/src/main/kotlin/exploration/navigation/MainNavGraph.kt
+++ app/exploration/src/main/kotlin/exploration/navigation/MainNavGraph.kt
@@ -3,40 +3,30 @@ package com.indramahkota.app.exploration.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.indramahkota.app.exploration.feature.catalogui.CatalogUIScreen
-import com.indramahkota.app.exploration.feature.catalogui.components.ButtonScreen
-import com.indramahkota.app.exploration.feature.catalogui.components.OthersScreen
-import com.indramahkota.app.exploration.feature.catalogui.components.TypographyScreen
-import com.indramahkota.app.exploration.feature.homebase.HomeBaseScreen
-import com.indramahkota.app.exploration.feature.splash.SplashScreen
+import com.indramahkota.app.exploration.feature.catalogui.navigation.catalogUINavigation
+import com.indramahkota.app.exploration.feature.homebase.navigation.homeBaseNavigation
+import com.indramahkota.app.exploration.feature.splash.navigation.splashNavigation
 import com.indramahkota.app.exploration.navigation.navigation.Screen
 
 @Composable
 fun MainNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Screen.Splash.route) {
-        composable(route = Screen.Splash.route) {
-            SplashScreen(navHostController = navController)
-        }
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        /**
+         * Splash Module Navigation
+         * */
+        splashNavigation(navController = navController)
 
-        composable(route = Screen.HomeBase.route) {
-            HomeBaseScreen(navHostController = navController)
-        }
+        /**
+         * Home Base Module Navigation
+         * */
+        homeBaseNavigation(navController = navController)
 
-        composable(route = Screen.CatalogUI.route) {
-            CatalogUIScreen(navHostController = navController)
-        }
-
-        composable(route = Screen.ButtonScreen.route) {
-            ButtonScreen(navHostController = navController)
-        }
-
-        composable(route = Screen.TypographyScreen.route) {
-            TypographyScreen(navHostController = navController)
-        }
-
-        composable(route = Screen.OthersScreen.route) {
-            OthersScreen(navHostController = navController)
-        }
+        /**
+         * Catalog UI Module Navigation
+         * */
+        catalogUINavigation(navController = navController)
     }
 }
