3158c1665e1e9317e6c7e28ecb5436f16a3520b0
Indra Mahkota <indramahkota1@gmail.com>
2023-08-23 21:58:51 +0700

    🔨 Fix SplashScreen with androidx core-splashscreen

--- app/designsystem/src/main/kotlin/com/indramahkota/android/designsystem/MainActivity.kt
+++ app/designsystem/src/main/kotlin/com/indramahkota/android/designsystem/MainActivity.kt
@@ -8,13 +8,14 @@ import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.indramahkota.android.core.designsystem.theme.AppTheme
+import com.indramahkota.android.core.ui.splashscreen.withSlideUpAnimation
 import com.indramahkota.android.designsystem.navigation.MainNavGraph
 
 class MainActivity : ComponentActivity() {
   private lateinit var navController: NavHostController
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    installSplashScreen()
+    installSplashScreen().withSlideUpAnimation()
     super.onCreate(savedInstanceState)
 
     // Turn off the decor fitting system windows, which allows us to handle insets,
